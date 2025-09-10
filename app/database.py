3744687491aafa6b3e5248595c9c3b0c9af1cea9
Artyom Vlasov <t0mi.v@yandex.ru>
2025-09-10 14:51:44 +0300

    update path to saving data and db

--- app/database.py
+++ app/database.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+import os
  
-SQLALCHEMY_DATABASE_URL = "sqlite:///./people.db"
+db_folder = "./app/database"
+os.makedirs(db_folder, exist_ok=True)
+
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{db_folder}/users.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
--- tests/conftest.py
+++ tests/conftest.py
@@ -30,9 +30,11 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app.database import Base, get_db
 from app.main import app
+ 
+db_folder = "./tests/database"
+os.makedirs(db_folder, exist_ok=True)
 
-# Create a new database session for testing
-SQLALCHEMY_DATABASE_URL = "sqlite:///./people.db"
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{db_folder}/users.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
