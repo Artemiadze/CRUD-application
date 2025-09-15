3025e7833b093e952aca1c3f199d8d4dc36974ce
Artyom Vlasov <t0mi.v@yandex.ru>
2025-09-15 08:53:27 +0300

    add user_id to logs

--- src/core/config.py
+++ src/core/config.py
@@ -9,6 +9,16 @@ class Settings(BaseSettings):
     DATABASE_URL: str = "sqlite:///./src/database/users.db"
 
 
+class UserLoggerAdapter(logging.LoggerAdapter):
+    """
+    Custom logger adapter to add contextual information to log records.
+    """
+
+    def process(self, msg, kwargs):
+        user_id = self.extra.get("user_id", "anon")
+        return f"[user_id={user_id}] {msg}", kwargs
+    
+
 # Configuring logging
 def setup_logging():
     logging.basicConfig(
@@ -21,5 +31,14 @@ def setup_logging():
     return logger
 
 
-logger = setup_logging()
+main_logger = setup_logging()
+
 settings = Settings()
+
+
+def get_user_logger(user_id: int | None = None):
+    """
+    Factory function to get a logger with user_id context.
+    If user_id is None, it defaults to 'non'.
+    """
+    return UserLoggerAdapter(main_logger, {"user_id": user_id})
