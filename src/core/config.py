29bbee8bb0551b3a346c163427f73d9542be80ae
Artyom Vlasov <t0mi.v@yandex.ru>
2025-09-15 08:23:36 +0300

    Setup logging configuration

--- src/core/config.py
+++ src/core/config.py
@@ -1,10 +1,25 @@
 import os
+import logging
 from pydantic_settings import BaseSettings
 
-
+# Creating directory for the database if it doesn't exist
 os.makedirs(os.path.dirname("src/database/users.db"), exist_ok=True)
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "sqlite:///./src/database/users.db"
 
+
+# Configuring logging
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        filename = "ServiceLogs.log",
+        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    )
+
+    logger = logging.getLogger("app")
+    return logger
+
+
+logger = setup_logging()
 settings = Settings()
